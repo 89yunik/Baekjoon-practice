c02d42954e0ee185d77c834395dc67e35aa74e83
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-09-13 17:39:35 +0900

    solving : 1039

---
+++ 1039.js
@@ -0,0 +1,7 @@
+// baekjoon 1039
+const fs = require('fs')
+let input = fs.readFileSync('./dev/stdin').toString().split('\r\n')
+
+const [N, K] = input[0].split(' ').map(e => parseInt(e))
+const result = 0
+console.log(result)
