4437109fb009782b59786a6d6b34555922e3172c
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-09-14 18:01:37 +0900

    solving : 1039

--- 1039.js
+++ 1039.js
@@ -3,5 +3,7 @@ const fs = require('fs')
 let input = fs.readFileSync('./dev/stdin').toString().split('\r\n')
 
 const [N, K] = input[0].split(' ').map(e => parseInt(e))
+const nArr = N.toString().split('')
+for (let i = 0; i < K; i++) {}
 const result = 0
 console.log(result)
