ce64d6dac14618480ca32a44fbac71335e39f17c
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-09-16 21:41:34 +0900

    solving : 1039 - greedy 불가능 확인

--- 1039.js
+++ 1039.js
@@ -5,16 +5,25 @@ let input = fs.readFileSync('./dev/stdin').toString().split('\r\n')
 let [N, K] = input[0].split(' ')
 K = parseInt(K)
 let result = -1
-if (N.length !== 1 && (N.length !== 2 || N[N.length - 1] != 0)) {
-  const nArr = N.split('').map(e => parseInt(e))
+if (N.length !== 1 && parseInt(N.slice(1, N.length)) !== 0) {
+  let nArr = N.split('').map(e => parseInt(e))
   for (let i = 0; i < nArr.length - 1; i++) {
     if (K === 0) {
       break
     }
-    const key = nArr[i]
     const max = Math.max(...nArr.slice(i + 1))
+    let key = nArr[i]
     if (key < max) {
-      const j = nArr.lastIndexOf(max) //error
+      let candidate = nArr.slice(i + 1).reduce((cnt, e) => cnt + (e === max), 0)
+      let subArr = nArr.slice(i, i + candidate)
+      const impossible = subArr.reduce((cnt, e) => cnt + (e === max), 0)
+      candidate = Math.min(candidate, K + impossible)
+      if (candidate > 1) {
+        subArr = nArr.slice(i, i + candidate).sort()
+        nArr = [...nArr.slice(0, i), ...subArr, ...nArr.slice(i + candidate)]
+        key = nArr[i]
+      }
+      const j = nArr.lastIndexOf(max)
       nArr[i] = max
       nArr[j] = key
       K--
