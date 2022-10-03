04280e6f78bd1f54e231347db641fb10cea7d48e
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-10-03 15:15:23 +0900

    solved : 1456

--- 1456.js
+++ 1456.js
@@ -1,28 +1,25 @@
 const fs = require('fs')
 const input = fs.readFileSync('./dev/stdin').toString().trim().split('\r\n')
-// const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 
-let [A, B] = input[0].split(' ').map(e => Number(e))
+const [A, B] = input[0].split(' ').map(e => Number(e))
 
 const max = Math.floor(Math.sqrt(B))
-const pNum = []
+const pNums = []
 const visited = new Array(max + 1).fill(false)
-visited[0] = true
-visited[1] = true
-let i = 2
 for (let i = 2; i < max + 1; i++) {
   if (visited[i]) continue
   visited[i] = true
-  pNum.push(i)
+  pNums.push(i)
   for (let j = 2; i * j < max + 1; j++) visited[i * j] = true
 }
 
 let answer = 0
-for (const prime of pNum) {
-  let candidate = prime * prime
+for (const pNum of pNums) {
+  let candidate = pNum * pNum
   while (candidate <= B) {
     if (candidate >= A) answer++
-    candidate *= prime
+    candidate *= pNum
   }
 }
 console.log(answer)
