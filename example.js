744e3a06ddfc5008ae84ba3fbfc09090f785df8e
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-10-04 22:27:31 +0900

    solved : 1039

--- example.js
+++ example.js
@@ -1,28 +1,37 @@
 const fs = require('fs')
-const input = fs.readFileSync('./dev/stdin').toString().trim().split('\r\n')
-// const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let input = fs.readFileSync('./dev/stdin').toString().trim().split('\r\n')
+//let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 
-let [A, B] = input[0].split(' ').map(e => Number(e))
-
-const max = Math.floor(Math.sqrt(B))
-const pNum = []
-const visited = new Array(max + 1).fill(false)
-visited[0] = true
-visited[1] = true
-let i = 2
-for (let i = 2; i < max + 1; i++) {
-  if (visited[i]) continue
-  visited[i] = true
-  pNum.push(i)
-  for (let j = 2; i * j < max + 1; j++) visited[i * j] = true
+function swap(string, [i, j]) {
+  return (
+    string.substring(0, i) +
+    string[j] +
+    string.substring(i + 1, j) +
+    string[i] +
+    string.substring(j + 1, string.length)
+  )
 }
-
-let answer = 0
-for (const prime of pNum) {
-  let candidate = prime * prime
-  while (candidate <= B) {
-    if (candidate >= A) answer++
-    candidate *= prime
+const [N, K] = input.shift().split(' ')
+let answer = -1
+const visited = new Array(Number(K) + 1).fill().map(_ => new Set(['-1']))
+const length = N.length
+const index = []
+for (let i = 0; i < length - 1; i++) {
+  for (let j = i + 1; j < length; j++) {
+    index.push([N, i, j, 0])
   }
 }
+const queue = [...index]
+while (queue.length) {
+  const [curr, i, j, swapCount] = queue.shift()
+  if (swapCount === Number(K)) continue
+  const next = swap(curr, [i, j])
+  if (next[0] === '0' || visited[swapCount + 1].has(next)) continue
+  visited[swapCount + 1].add(next)
+  index.forEach(([curr, i, j]) => {
+    queue.push([next, i, j, swapCount + 1])
+  })
+}
+answer = Math.max(...visited[K])
+
 console.log(answer)
