58f4648ba5ab7b0bb804c1bd0a80da2a91894663
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-10-04 22:27:25 +0900

    solved : 1039

--- 1039.js
+++ 1039.js
@@ -1,12 +1,37 @@
-// baekjoon 1039
 const fs = require('fs')
-let input = fs.readFileSync('./dev/stdin').toString().split('\r\n')
+let input = fs.readFileSync('./dev/stdin').toString().trim().split('\r\n')
+//let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 
-let [N, K] = input[0].split(' ')
-K = parseInt(K)
-let result = -1
-if (N.length !== 1 && parseInt(N.slice(1, N.length)) !== 0) {
-  const arrayN = N.split('')
-  console.log(arrayN)
+function swap(string, [i, j]) {
+  return (
+    string.substring(0, i) +
+    string[j] +
+    string.substring(i + 1, j) +
+    string[i] +
+    string.substring(j + 1, string.length)
+  )
 }
-console.log(result)
+const [N, K] = input.shift().split(' ')
+let answer = -1
+const visited = new Array(Number(K) + 1).fill().map(_ => new Set(['-1']))
+const length = N.length
+const index = []
+for (let i = 0; i < length - 1; i++) {
+  for (let j = i + 1; j < length; j++) {
+    index.push([N, i, j, 0])
+  }
+}
+const queue = [...index]
+while (queue.length) {
+  const [curr, i, j, swapCount] = queue.shift()
+  if (swapCount === Number(K)) continue
+  const next = swap(curr, [i, j])
+  if (next[0] === '0' || visited[swapCount + 1].has(next)) continue
+  visited[swapCount + 1].add(next)
+  index.forEach(([curr, i, j]) => {
+    queue.push([next, i, j, swapCount + 1])
+  })
+}
+answer = Math.max(...visited[K])
+
+console.log(answer)
