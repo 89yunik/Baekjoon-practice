74a44c241742811212f7aa9f06b5091366028f76
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-10-10 21:24:53 +0900

    solved : 2447

--- recursion/2447.js
+++ recursion/2447.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const input = fs.readFileSync('./dev/stdin').toString().trim().split('\r\n')
 //const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 
-function stars(N, arr, [x, y]) {
+function stars(N, [x, y]) {
   if (N === 3) {
     for (let i = x; i <= x + 2; i++) {
       for (let j = y; j <= y + 2; j++) {
@@ -14,14 +14,14 @@ function stars(N, arr, [x, y]) {
     for (let i = 0; i <= 2; i++) {
       for (let j = 0; j <= 2; j++) {
         if (i === 1 && j === 1) continue
-        stars(N / 3, arr, [x + (N / 3) * i, y + (N / 3) * j])
+        stars(N / 3, [x + (N / 3) * i, y + (N / 3) * j])
       }
     }
   }
 }
 const N = Number(input[0])
 const arr = new Array(N).fill().map(_ => new Array(N).fill(' '))
-stars(N, arr, [0, 0])
+stars(N, [0, 0])
 let answer = ''
 for (let row of arr) answer += row.join('') + '\n'
 answer = answer.replace(/\n$/, '')
