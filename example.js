24561a5d0cac92367ee86f631f9e0028061f5405
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-10-11 22:06:31 +0900

    setting : example

--- example.js
+++ example.js
@@ -2,27 +2,15 @@ const fs = require('fs')
 const input = fs.readFileSync('./dev/stdin').toString().trim().split('\r\n')
 //const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 
-function stars(N, [x, y]) {
-  if (N === 3) {
-    for (let i = x; i <= x + 2; i++) {
-      for (let j = y; j <= y + 2; j++) {
-        if (i === x + 1 && j === y + 1) continue
-        arr[i][j] = '*'
-      }
-    }
-  } else {
-    for (let i = 0; i <= 2; i++) {
-      for (let j = 0; j <= 2; j++) {
-        if (i === 1 && j === 1) continue
-        stars(N / 3, [x + (N / 3) * i, y + (N / 3) * j])
-      }
-    }
-  }
-}
 const N = Number(input[0])
-const arr = new Array(N).fill().map(_ => new Array(N).fill(' '))
-stars(N, [0, 0])
-let answer = ''
-for (let row of arr) answer += row.join('') + '\n'
-answer = answer.replace(/\n$/, '')
-console.log(answer)
+const min = input[1].split(' ').map(e => +e)
+for (let i = 2; i <= N; i++) {
+  const [R, G, B] = input[i].split(' ').map(e => +e)
+  const temp1 = R + Math.min(min[1], min[2])
+  const temp2 = G + Math.min(min[0], min[2])
+  const temp3 = B + Math.min(min[0], min[1])
+  min[0] = temp1
+  min[1] = temp2
+  min[2] = temp3
+}
+console.log(Math.min(...min))
