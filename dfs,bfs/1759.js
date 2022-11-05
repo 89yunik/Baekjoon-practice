e1923a3318d5cdde3a1ef51a3f0b76f8b3a17468
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-11-05 13:24:02 +0900

    solved : many

--- dfs,bfs/1759.js
+++ dfs,bfs/1759.js
@@ -4,7 +4,7 @@ const input = fs.readFileSync('./dev/stdin').toString().trim().split('\r\n')
 const [L, C] = input[0].split(' ').map(Number)
 const [A, B] = [input[1].split(' ').sort(), []]
 function T(S) {
-  const v = S.split('').reduce((p, c) => p + +/[aeiou]/.T(c), 0)
+  const v = S.split('').reduce((p, c) => p + +/[aeiou]/.test(c), 0)
   return v < 1 || L - v < 2 ? 0 : 1
 }
 function dfs(S, x) {
@@ -14,5 +14,5 @@ function dfs(S, x) {
   }
   for (let i = x + 1; i < C; i++) dfs(S + A[i], i)
 }
-for (let i = 0; i < C; i++) dfs(A[i], i)
+for (const i in A) dfs(A[i], +i)
 console.log(B.join('\n'))
