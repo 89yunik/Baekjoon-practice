70400f7efb883fabd2d053183a4a332746d3e5fa
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-11-21 00:52:28 +0900

    solved : 1697

--- Depth-first Search, Breadth-first Search/BFS/7576.js
+++ Depth-first Search, Breadth-first Search/BFS/7576.js
@@ -36,27 +36,28 @@ class Queue {
 function sol() {
   const [[M, N], A, Q] = [I[0].split(' ').map(Number), [], new Queue()]
   for (let i = 1; i <= N; i++) A.push(I[i].split(' ').map(Number))
-  let num = 0
   for (let i = 0; i < N; i++)
-    for (let j = 0; j < M; j++) {
-      if (A[i][j] == -1) num++
-      else if (A[i][j] == 1) {
+    for (let j = 0; j < M; j++)
+      if (A[i][j] == 1) {
         A[i][j] = 0
         Q.push([i, j, 0])
       }
-    }
+  let [cnt, x] = [Q.size, -1]
   while (Q.size) {
-    const [i, j, n] = Q.pop()
+    const [i, j] = Q.pop()
+    if (!cnt--) {
+      cnt = Q.size
+      x++
+    }
     if (A[i][j]) continue
     A[i][j] = 1
-    if (++num == M * N) return n
-    if (i && !A[i - 1][j]) Q.push([i - 1, j, n + 1])
-    if (j && !A[i][j - 1]) Q.push([i, j - 1, n + 1])
-    if (i + 1 < N && !A[i + 1][j]) Q.push([i + 1, j, n + 1])
-    if (j + 1 < M && !A[i][j + 1]) Q.push([i, j + 1, n + 1])
+    i && Q.push([i - 1, j])
+    j && Q.push([i, j - 1])
+    i + 1 < N && Q.push([i + 1, j])
+    j + 1 < M && Q.push([i, j + 1])
   }
   for (let i = 0; i < N; i++)
     for (let j = 0; j < M; j++) if (!A[i][j]) return -1
-  return 0
+  return x
 }
 console.log(sol())
