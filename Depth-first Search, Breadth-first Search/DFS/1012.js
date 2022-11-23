15c83b7086df648a5a0fe3e45c5ba6fd58107896
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-11-23 12:07:49 +0900

    solved : 1074

--- Depth-first Search, Breadth-first Search/DFS/1012.js
+++ Depth-first Search, Breadth-first Search/DFS/1012.js
@@ -8,28 +8,43 @@ const [T, O] = [+I[0], []]
 let i = 1
 while (i < I.length) {
   const [M, N, K] = I[i++].split(' ').map(Number)
-  const A = Array.from({length: M}, _ => Array(N).fill(0))
-  const V = Array.from({length: M}, _ => Array(N).fill(0))
-  function dfs(x, y) {
-    if (V[x][y] || !A[x][y]) return
-    V[x][y] = 1
-    x && dfs(x - 1, y)
-    y && dfs(x, y - 1)
-    x < M - 1 && dfs(x + 1, y)
-    y < N - 1 && dfs(x, y + 1)
+  const L = M * N
+  const [A, V] = [Array(L).fill(0), Array(L).fill(0)]
+  for (let j = 0; j < K; j++) {
+    const [x, y] = I[i + j].split(' ').map(Number)
+    A[x * N + y]++
   }
-  for (j = 0; j < K; j++) {
-    const [x, y] = I[i + j].split(' ')
-    A[x][y] = 1
+  function dfs(x) {
+    if (V[x] || !A[x]) return
+    V[x] = 1
+    const [next, rest] = [[x - N, x + N], x % N]
+    rest && next.push(x - 1)
+    rest != N - 1 && next.push(x + 1)
+    for (let i = 0; i < next.length; i++)
+      if (next[i] >= 0 && next[i] < L) dfs(next[i])
   }
   let o = 0
-  for (let i = 0; i < M; i++)
-    for (let j = 0; j < N; j++)
-      if (!V[i][j] && A[i][j]) {
-        o++
-        dfs(i, j)
-      }
+  for (let j = 0; j < L; j++) if (!V[j] && A[j] && ++o) dfs(j)
   O.push(o)
   i += K
+  // const A = Array.from({length: M}, _ => Array(N).fill(0))
+  // const V = Array.from({length: M}, _ => Array(N).fill(0))
+  // function dfs(x, y) {
+  //   if (V[x][y] || !A[x][y]) return
+  //   V[x][y] = 1
+  //   x && dfs(x - 1, y)
+  //   y && dfs(x, y - 1)
+  //   x < M - 1 && dfs(x + 1, y)
+  //   y < N - 1 && dfs(x, y + 1)
+  // }
+  // for (j = 0; j < K; j++) {
+  //   const [x, y] = I[i + j].split(' ')
+  //   A[x][y] = 1
+  // }
+  // let o = 0
+  // for (let i = 0; i < M; i++)
+  //   for (let j = 0; j < N; j++) if (!V[i][j] && A[i][j] && ++o) dfs(i, j)
+  // O.push(o)
+  // i += K
 }
 console.log(O.join('\n'))
