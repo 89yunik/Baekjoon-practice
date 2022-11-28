d73ad90e144e6f8abd9203824460c0d5e495732d
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-11-28 12:57:21 +0900

    solved : 14889

--- Prefix Sum/11659.js
+++ Prefix Sum/11659.js
@@ -21,32 +21,3 @@ for (let i = 2; i < M + 2; i++) {
   O.push(A[e] - A[s - 1])
 }
 console.log(O.join('\n'))
-// const [N, S] = [+I[0], []]
-// for (let i = 1; i <= N; i++) S.push(I[i].split(' ').map(Number))
-// const [A, B] = [new Set(), new Set()]
-// let o = Infinity
-// function dfs(i, diff) {
-//   if (A.size + B.size == N) {
-//     o = Math.min(o, Math.abs(diff))
-//     return
-//   }
-//   for (let j = i; j < N; j++)
-//     if (!A.has(j) && !B.has(j)) {
-//       if (A.size < N / 2) {
-//         let x = 0
-//         for (let n of A) x += S[n][j] + S[j][n]
-//         A.add(j)
-//         dfs(i + 1, diff + x)
-//         A.delete(j)
-//       }
-//       if (B.size < N / 2) {
-//         let x = 0
-//         for (let n of B) x += S[n][j] + S[j][n]
-//         B.add(j)
-//         dfs(i + 1, diff - x)
-//         B.delete(j)
-//       }
-//     }
-// }
-// dfs(0, 0)
-// console.log(o)
