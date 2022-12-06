081788705b3478fbfec77996edbde4f1ce842728
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-12-06 22:29:18 +0900

    solved : 6064

--- String/5525.js
+++ String/5525.js
@@ -5,12 +5,6 @@ let [N, M, I] = require('fs')
   .split('\r\n')
 // let I = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
 I = I.replaceAll('I', 1).replaceAll('O', 0)
-// function F(s) {
-//   let [i, num] = [0, 0]
-//   for (; i < I.length; i++) if (I[s + i] == i % 2) break
-//   if (i > 2 * N) num = Math.ceil(i / 2 - N)
-//   return [i, num]
-// }
 let [i, cnt] = [0, 0]
 while (i < I.length) {
   let j = 0
