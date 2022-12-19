0db7c42a7a4188df6922a2d211701f8a1d88788d
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-12-19 19:27:16 +0900

    solved : 17404

--- Dynamic Programming/1149.js
+++ Dynamic Programming/1149.js
@@ -1,16 +1,14 @@
-const fs = require('fs')
-const input = fs.readFileSync('./dev/stdin').toString().trim().split('\r\n')
-//const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
-
-const N = Number(input[0])
-const min = input[1].split(' ').map(e => +e)
-for (let i = 2; i <= N; i++) {
-  const [R, G, B] = input[i].split(' ').map(e => +e)
-  const temp1 = R + Math.min(min[1], min[2])
-  const temp2 = G + Math.min(min[0], min[2])
-  const temp3 = B + Math.min(min[0], min[1])
-  min[0] = temp1
-  min[1] = temp2
-  min[2] = temp3
-}
-console.log(Math.min(...min))
+let [N, ...I] = require('fs')
+  .readFileSync('./dev/stdin')
+  .toString()
+  .split('\r\n')
+  .map(e => e.split(' ').map(Number))
+// let [N, ...I] = require('fs').readFileSync('/dev/stdin').toString().split('\n').map(e => e.split(' ').map(Number))
+const M = Math.min
+for (let i = 1; i < +N; i++)
+  for (let j = 0; j < 3; j++) {
+    const T = [...I[i - 1]]
+    T.splice(j, 1)
+    I[i][j] += M(...T)
+  }
+console.log(M(...I[+N - 1]))
