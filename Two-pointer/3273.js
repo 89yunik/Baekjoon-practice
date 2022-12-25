503110a85aabe8e9a1f8334ee46b6719248cca69
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-12-25 20:35:15 +0900

    solved : 3273

--- Two-pointer/3273.js
+++ Two-pointer/3273.js
@@ -4,7 +4,7 @@ let [[n], a, [x]] = require('fs')
   .trim()
   .split('\r\n')
   .map(e => e.split(' ').map(Number))
-// let I = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(e => e.split(' ').map(Number))
+// let [[n], a, [x]] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(e => e.split(' ').map(Number))
 a = a.sort((a, b) => a - b)
 let [j, o] = [n - 1, 0]
 for (let i = 0; i < n; i++) {
