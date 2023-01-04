1ee9faa635e385a7a1ef794f63dfb0961062051f
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2023-01-04 17:10:27 +0900

    solving : 1450

--- greedy/1105.js
+++ greedy/1105.js
@@ -1,12 +1,13 @@
-const fs = require('fs')
-const input = fs.readFileSync('./dev/stdin').toString().trim().split('\r\n')
-// const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
-
-let [L, R] = input[0].split(' ')
-let output = 0
+const [L, R] = require('fs')
+  .readFileSync('./dev/stdin')
+  .toString()
+  .trim()
+  .split(' ')
+// const [L, R] = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ')
+let o = 0
 if (L.length == R.length)
   for (let i = 0; i < L.length; i++) {
     if (L[i] != R[i]) break
-    if (L[i] == '8') output++
+    if (L[i] == '8') o++
   }
-console.log(output)
+console.log(o)
