cd8ce803d258c6c180a76b2edb52e85a1ca7c3d3
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2023-01-08 16:09:44 +0900

    solved : 17299

--- Dynamic Programming/17298.js
+++ Dynamic Programming/17298.js
@@ -3,12 +3,10 @@ let [[N], A] = require('fs')
   .toString()
   .split('\r\n')
   .map(e => e.split(' ').map(Number))
-const B = new Map()
-A.forEach(e => B.set(e, (B.get(e) ?? 0) + 1))
 const dp = Array(N).fill(-1)
 for (let i = N - 2; i >= 0; i--) {
   let j = i + 1
-  while (B.get(A[i]) >= B.get(A[j])) j = dp[j]
+  while (j && A[i] >= A[j]) j = dp[j]
   dp[i] = j
 }
 console.log(dp.map(e => (e > 0 ? A[e] : e)).join(' '))
