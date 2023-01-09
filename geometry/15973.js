3a71b45313306172649274e9ad2de827af77abe1
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2023-01-09 17:18:46 +0900

    solved : 1715

--- geometry/15973.js
+++ geometry/15973.js
@@ -1,32 +1,15 @@
-// const fs = require('fs')
-// const input = fs.readFileSync('./dev/stdin').toString().trim().split('\r\n')
-// //const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
-
-// function state(x1, y1, x2, y2, x3, y3, x4, y4) {
-//   if ((x3 === x2 || x4 === x1) && (y3 === y2 || y4 === y1)) return 'POINT'
-//   if (
-//     ((x3 === x2 || x4 === x1) && y4 > y1 && y3 < y2) ||
-//     ((y3 === y2 || y4 === y1) && x4 > x1 && x3 < x2)
-//   )return 'LINE'
-//   if (x3 > x2 || y3 > y2 || x4 < x1 || y4 < y1) return 'NULL'
-//   return 'FACE'
-// }
-// const [x1, y1, x2, y2] = input[0].split(' ').map(e => +e)
-// const [x3, y3, x4, y4] = input[1].split(' ').map(e => +e)
-// const answer = state(x1, y1, x2, y2, x3, y3, x4, y4)
-// console.log(answer)
-
-const readline = require('readline')
-
-const rl = readline.createInterface({
+// const [[x1, y1, x2, y2], [x3, y3, x4, y4]] = require('fs').readFileSync(0).toString().split('\n').map(e => e.split(' ').map(Number))
+// if ((x3 == x2 || x4 == x1) && (y3 == y2 || y4 == y1)) console.log('POINT')
+// else if (((x3 == x2 || x4 == x1) && y4 > y1 && y3 < y2) || ((y3 == y2 || y4 == y1) && x4 > x1 && x3 < x2)) console.log('LINE')
+// else if (x3 > x2 || y3 > y2 || x4 < x1 || y4 < y1) console.log('NULL')
+// else console.log('FACE')
+const rl = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout,
 })
-
-let input = []
-
+let I = []
 rl.on('line', function (line) {
-  input.push(line)
+  I.push(line)
 }).on('close', function () {
   function state(x1, y1, x2, y2, x3, y3, x4, y4) {
     if ((x3 === x2 || x4 === x1) && (y3 === y2 || y4 === y1)) return 'POINT'
@@ -38,9 +21,8 @@ rl.on('line', function (line) {
     if (x3 > x2 || y3 > y2 || x4 < x1 || y4 < y1) return 'NULL'
     return 'FACE'
   }
-  const [x1, y1, x2, y2] = input[0].split(' ').map(e => +e)
-  const [x3, y3, x4, y4] = input[1].split(' ').map(e => +e)
-  const answer = state(x1, y1, x2, y2, x3, y3, x4, y4)
-  console.log(answer)
+  const [x1, y1, x2, y2] = I[0].split(' ').map(Number)
+  const [x3, y3, x4, y4] = I[1].split(' ').map(Number)
+  console.log(state(x1, y1, x2, y2, x3, y3, x4, y4))
   process.exit()
 })
--- recursion/1074.js
+++ recursion/1074.js
@@ -1,17 +1,15 @@
-const I = require('fs')
-  .readFileSync('./dev/stdin')
+const [N, r, c] = require('fs')
+  .readFileSync('/dev/stdin')
   .toString()
-  .trim()
-  .split('\r\n')
-// const I = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
-const [N, r, c] = I[0].split(' ').map(Number)
-function quartered(N, r, c) {
+  .split(' ')
+  .map(Number)
+function Q(N, r, c) {
   if (N == 1) return (r % 2) * 2 + (c % 2)
   const l = Math.pow(2, N - 1)
-  const [a, b] = [parseInt(r / l), parseInt(c / l)]
+  const [a, b] = [~~(r / l), ~~(c / l)]
   const x = 2 * a + b
   if (a) r -= l
   if (b) c -= l
-  return x * l * l + quartered(N - 1, r, c)
+  return x * l * l + Q(N - 1, r, c)
 }
-console.log(quartered(N, r, c))
+console.log(Q(N, r, c))
