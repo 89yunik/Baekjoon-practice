4094ba56fd1dfb66a40009dfede2afea46e2f2a7
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2023-01-10 16:54:27 +0900

    solved : 11066

--- Binary Search/1715.js
+++ Binary Search/1715.js
@@ -8,13 +8,13 @@ I.sort((a, b) => b - a)
 let o = 0
 while (I.length > 1) {
   const [x, y] = [I.pop(), I.pop()]
-  const [z, L] = [x + y, I.length]
+  const [z, L] = [x + y, I.length - 1]
   let [l, r] = [0, L]
   while (l < r) {
     const mid = ~~((l + r) / 2)
     z > I[mid] ? (r = mid) : (l = mid + 1)
   }
-  I.splice(z > I[L - 1] ? r : L - 1, 0, z)
+  I.splice(z > I[L] ? r : L, 0, z)
   o += z
 }
 console.log(o)
