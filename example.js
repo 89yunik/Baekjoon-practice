07ce6541c2144fe61516dafb1a83c45184f89a6a
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2023-01-21 13:27:56 +0900

    solved : 2293

--- example.js
+++ example.js
@@ -1,24 +1,13 @@
-let [[N], I] = require('fs')
+let [[n, k], ...I] = require('fs')
   .readFileSync('./dev/stdin')
   .toString()
   .trim()
   .split('\r\n')
   .map(e => e.split(' ').map(Number))
-I.sort((a, b) => a - b)
-let o = 0
-for (let i = 0; i < N; i++) {
-  let [l, r] = [0, N - 1]
-  while (l < r)
-    if (i == l || I[l] + I[r] < I[i]) l++
-    else if (i == r || I[l] + I[r] > I[i]) r--
-    else if (++o) break
-}
-console.log(o)
-// I.sort((a, b) => a - b)
-// let o = 0
-// for (let i = 0; i < N; i++)
-//   l: for (let j = 0; j < N; j++)
-//     for (let k = j + 1; k < N; k++)
-//       if (i == j || I[j] + I[k] > I[i]) break
-//       else if (i != k && I[j] + I[k] == I[i] && ++o) break l
-// console.log(o)
+I = I.flat()
+const dp = [...Array(n)].map(_ => Array(k + 1).fill(0))
+for (let i = 0; i <= k; i += I[0]) dp[0][i]++
+for (let i = 1; i < n; i++)
+  for (let j = 0; j <= k; j++)
+    for (let x = 0; x <= j; x += I[i]) dp[i][j] += dp[i - 1][j - x]
+console.log(dp[n - 1][k])
