0ccdc5c94569494fd2e3fae7d419f19485cfd113
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2023-01-22 21:44:48 +0900

    solved : 2075

--- example.js
+++ example.js
@@ -1,13 +1,7 @@
-let [[n, k], ...I] = require('fs')
+let [[N], ...I] = require('fs')
   .readFileSync('./dev/stdin')
   .toString()
   .trim()
   .split('\r\n')
   .map(e => e.split(' ').map(Number))
-I = I.flat()
-const dp = [...Array(n)].map(_ => Array(k + 1).fill(0))
-for (let i = 0; i <= k; i += I[0]) dp[0][i]++
-for (let i = 1; i < n; i++)
-  for (let j = 0; j <= k; j++)
-    for (let x = 0; x <= j; x += I[i]) dp[i][j] += dp[i - 1][j - x]
-console.log(dp[n - 1][k])
+console.log(I.flat().sort((a, b) => b - a)[N - 1])
