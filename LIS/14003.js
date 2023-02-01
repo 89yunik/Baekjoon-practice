ffd824df2c639ff8aa1df434a27bcb7fb3881fb7
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2023-02-01 13:58:36 +0900

    solved : 14003

--- LIS/14003.js
+++ LIS/14003.js
@@ -16,9 +16,5 @@ for (let x of A) {
   C.push([l, x])
 }
 let x = B.length - 1
-for (let i = N - 1; i >= 0; i--)
-  if (C[i][0] == x) {
-    B[x] = C[i][1]
-    x--
-  }
+for (let i = N - 1; i >= 0; i--) if (C[i][0] == x && (B[x] = C[i][1])) x--
 console.log(`${B.length}\n${B.join(' ')}`)
