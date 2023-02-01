67041516a7dd6d8572f8a88ed4389cec34f5314f
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2023-02-01 13:52:41 +0900

    solved : 14003

--- LIS/14003.js
+++ LIS/14003.js
@@ -6,16 +6,14 @@ let [[N], A] = require('fs')
   .map(e => e.split(' ').map(Number))
 // let [[N], A] = [[1000000], Array(1000000).fill(1)]
 const [B, C] = [[], []]
-B.push(A[0])
-C.push([0, A[0]])
-for (let i = 1; i < N; i++) {
+for (let x of A) {
   let [l, r] = [0, B.length]
   while (l < r) {
     const mid = ~~((l + r) / 2)
-    B[mid] < A[i] ? (l = mid + 1) : (r = mid)
+    B[mid] < x ? (l = mid + 1) : (r = mid)
   }
-  B[l] = A[i]
-  C.push([l, A[i]])
+  B[l] = x
+  C.push([l, x])
 }
 let x = B.length - 1
 for (let i = N - 1; i >= 0; i--)
