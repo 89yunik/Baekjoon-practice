cc53350da7cf7f5b45555ca4ac0e6d2ebc009c51
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2023-02-02 13:27:46 +0900

    solved : 2096

--- example.js
+++ example.js
@@ -1,17 +1,23 @@
-let [[N], A] = require('fs')
+let [[N], ...I] = require('fs')
   .readFileSync('./dev/stdin')
   .toString()
   .trim()
-  .split('\r\n')
+  .split('\n')
   .map(e => e.split(' ').map(Number))
-// let [[N], A] = [[1000000], Array(1000000).fill(1)]
-const B = []
-for (let x of A) {
-  let [l, r] = [0, B.length]
-  while (l < r) {
-    const mid = ~~((l + r) / 2)
-    B[mid] < x ? (l = mid + 1) : (r = mid)
-  }
-  B[l] = x
+let [dp1, dp2] = [
+  [0, 0, 0],
+  [0, 0, 0],
+]
+for (const [a, b, c] of I) {
+  dp1 = [
+    Math.max(dp1[0], dp1[1]) + a,
+    Math.max(dp1[0], dp1[1], dp1[2]) + b,
+    Math.max(dp1[1], dp1[2]) + c,
+  ]
+  dp2 = [
+    Math.min(dp2[0], dp2[1]) + a,
+    Math.min(dp2[0], dp2[1], dp2[2]) + b,
+    Math.min(dp2[1], dp2[2]) + c,
+  ]
 }
-console.log(B.length)
+console.log(Math.max(...dp1), Math.min(...dp2))
