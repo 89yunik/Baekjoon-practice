40e2b99fd9417b2e3b75f651c484fdd18c9c6781
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2023-02-09 10:36:10 +0900

    solved : 27211

---
+++ Graph Traversal/DFS/27211.js
@@ -0,0 +1,26 @@
+let [[N, M], ...I] = `${require('fs').readFileSync('./dev/stdin')}`.trim()
+  .split`\n`.map(e => e.split(' ').map(Number))
+let [V, o] = [[...Array(N)].map(_ => Array(M).fill(0)), 0]
+function dfs(sx, sy) {
+  const stack = [[sx, sy]]
+  while (stack.length) {
+    const [x, y] = stack.pop()
+    V[x][y]++
+    const next = [
+      [x - 1, y],
+      [x, y - 1],
+      [x, y + 1],
+      [x + 1, y],
+    ]
+    for (let [nx, ny] of next) {
+      if (nx == N) nx = 0
+      if (nx == -1) nx = N - 1
+      if (ny == M) ny = 0
+      if (ny == -1) ny = M - 1
+      if (!I[nx][ny] && !V[nx][ny]) stack.push([nx, ny])
+    }
+  }
+}
+for (let i = 0; i < N; i++)
+  for (let j = 0; j < M; j++) if (!I[i][j] && !V[i][j] && ++o) dfs(i, j)
+console.log(o)
--- example.js
+++ example.js
@@ -1,23 +1,4 @@
-let [N, M, K] = `${require('fs').readFileSync('./dev/stdin')}`.split` `.map(
-  Number,
-)
-let dp = [...Array(N + 1)].map(_ => Array(M + 1).fill(0n))
-for (let i = 1; i <= N; i++) dp[i][0] = 1n
-for (let i = 1; i <= M; i++) dp[0][i] = 1n
-for (let i = 1; i <= N; i++)
-  for (let j = 1; j <= M; j++) dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
-let [o, x, y] = ['', N, M]
-if (dp[N][M] < K) o = -1
-else {
-  while (x && y)
-    if (dp[x - 1][y] < K) {
-      o += 'z'
-      K -= Number(dp[x - 1][y])
-      y--
-    } else {
-      o += 'a'
-      x--
-    }
-  x ? (o += 'a'.repeat(x)) : (o += 'z'.repeat(y))
-}
-console.log(o)
+const [D, H, W] = `${require('fs').readFileSync('./dev/stdin')}`.trim()
+  .split` `.map(Number)
+const d = (H ** 2 + W ** 2) ** 0.5
+console.log(~~((D * H) / d), ~~((D * W) / d))
