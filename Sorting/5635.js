6aabe25f18498a23eeed074d228c31d9f6b706d6
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2023-02-16 10:24:04 +0900

    solved : 1652

--- Sorting/5635.js
+++ Sorting/5635.js
@@ -1,4 +1,2 @@
-let [, ...I] = `${require('fs').readFileSync('./dev/stdin')}`.trim()
-  .split`\r\n`.map(e => e.split` `)
-I.sort((a, b) => b[3] - a[3] || b[2] - a[2] || b[1] - a[1])
-console.log(I[0][0], I[I.length - 1][0])
+let [, ...I] = `${require('fs').readFileSync('./dev/stdin')}`.trim().split`\r\n`
+console.log(I)
