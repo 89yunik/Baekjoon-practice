005351c8f425fe832b47bac9c7481b6b8041f051
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2023-02-25 16:52:12 +0900

    solved : 순위 검색

--- Data Structures/Heap/11286.js
+++ Data Structures/Heap/11286.js
@@ -1,75 +1,43 @@
-const fs = require('fs')
-let input = fs.readFileSync('./dev/stdin').toString().trim().split('\r\n')
-// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
-
+let [, ...I] = `${require('fs').readFileSync('./dev/stdin')}`.trim()
+  .split`\r\n`.map(Number)
 class Heap {
-  constructor() {
-    this.nodes = []
+  constructor(e) {
+    this.A = []
+    this.check = e
   }
-  compare(a, b) {
-    if (Math.abs(a) < Math.abs(b)) {
-      return true
-    } else if (Math.abs(a) === Math.abs(b) && a < b) {
-      return true
-    } else {
-      return false
-    }
-  }
-  insert(data) {
-    this.nodes.push(data)
-    let child = this.nodes.length - 1
-    let parent = Math.floor((child - 1) / 2)
-    while (this.compare(this.nodes[child], this.nodes[parent]) && child > 0) {
-      const temp = this.nodes[parent]
-      this.nodes[parent] = this.nodes[child]
-      this.nodes[child] = temp
-      child = parent
-      parent = Math.floor((child - 1) / 2)
+  push(e) {
+    const A = this.A
+    A.push(e)
+    let c = A.length - 1
+    while (c) {
+      const p = ~~((c - 1) / 2)
+      if (this.check(A[p], A[c])) break
+      ;[A[p], A[c]] = [A[c], A[p]]
+      c = p
     }
   }
-  delete() {
-    const end = this.nodes.pop()
-    if (!this.nodes.length) {
-      return end ? end : 0
+  pop() {
+    const A = this.A
+    const [x, last] = [A[0], A.pop()]
+    const len = A.length
+    if (len) A[0] = last
+    let p = 0
+    while (p < (len - 1) / 2) {
+      const [l, r] = [p * 2 + 1, p * 2 + 2]
+      const c = this.check(A[r], A[l]) ? r : l
+      if (this.check(A[p], A[c])) break
+      ;[A[p], A[c]] = [A[c], A[p]]
+      p = c
     }
-    const min = this.nodes.splice(0, 1, end)
-    let parent = 0
-    let child = this.compare(
-      this.nodes[(parent + 1) * 2],
-      this.nodes[(parent + 1) * 2 - 1],
-    )
-      ? (parent + 1) * 2
-      : (parent + 1) * 2 - 1
-    while (
-      (parent + 1) * 2 - 1 < this.nodes.length &&
-      this.compare(this.nodes[child], this.nodes[parent])
-    ) {
-      const temp = this.nodes[parent]
-      this.nodes[parent] = this.nodes[child]
-      this.nodes[child] = temp
-      parent = child
-      child = this.compare(
-        this.nodes[(parent + 1) * 2],
-        this.nodes[(parent + 1) * 2 - 1],
-      )
-        ? (parent + 1) * 2
-        : (parent + 1) * 2 - 1
-    }
-    return min
+    return x
   }
 }
-
-const N = input.shift()
-let answer = ''
-const heap = new Heap()
-for (let num of input) {
-  num = Number(num)
-  if (num === 0) {
-    const min = heap.delete()
-    answer += min + '\n'
-  } else {
-    heap.insert(num)
-  }
-}
-const index = answer.lastIndexOf('\n')
-console.log(answer.substring(0, index))
+let O = []
+const H = new Heap((a, b) => {
+  const [A, B] = [Math.abs(a), Math.abs(b)]
+  return A < B || (A == B && a < b)
+})
+for (let n of I)
+  if (n) H.push(n)
+  else O.push(H.pop() ?? 0)
+console.log(O.join`\n`)
--- example.js
+++ example.js
@@ -1,13 +1,3 @@
-let [[N, K], ...I] = `${require('fs').readFileSync('./dev/stdin')}`.trim()
-  .split`\n`.map(e => e.split` `.map(Number))
-const dp = [...Array(N)].map(_ => Array(K + 1).fill(Infinity))
-const l = (a, b) => Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1])
-dp[0][0] = 0
-for (let i = 1; i < N; i++)
-  for (let j = 0; j <= K; j++)
-    for (let k = 0; k < i && k <= j; k++)
-      dp[i][j] = Math.min(
-        dp[i][j],
-        dp[i - k - 1][j - k] + l(I[i - k - 1], I[i]),
-      )
-console.log(dp[N - 1][K])
+let [, ...I] = `${require('fs').readFileSync('./dev/stdin')}`.trim()
+  .split`\n`.map(e => e.split` `.map(Number).sort((a, b) => a - b))
+console.log(I.map(([a, b, c]) => (a + b) ** 2 + c ** 2).join`\n`)
