4e48c52b0587485fa80d7a6310e58ecf00393b4c
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2023-02-28 21:32:05 +0900

    solved : 1173 - start python

--- example.js
+++ example.js
@@ -1,26 +1,30 @@
-let [, ...I] = `${require('fs').readFileSync('./dev/stdin')}`.trim()
-  .split`\n`.map(e => e.split` `.map(Number))
+let I = `${require('fs').readFileSync('./dev/stdin')}`.trim().split`\r\n`.map(
+  e => e.split` `,
+)
 console.log(
-  I.map(e => {
-    function score(x, y) {
-      const r = x ** 2 + y ** 2
-      return r > 225
-        ? 0
-        : r > 144
-        ? 20
-        : r > 81
-        ? 40
-        : r > 36
-        ? 60
-        : r > 9
-        ? 80
-        : 100
-    }
-    let [a, b] = [0, 0]
-    for (let i = 0; i < 3; i++) a += score(e[2 * i], e[2 * i + 1])
-    for (let i = 3; i < 6; i++) b += score(e[2 * i], e[2 * i + 1])
-    return `SCORE: ${a} to ${b}, ${
-      a > b ? `PLAYER 1 WINS.` : a < b ? `PLAYER 2 WINS.` : `TIE.`
-    }`
-  }).join`\n`,
+  I.reduce(
+    (a, [, b, c]) => [
+      a[0] + (c == 'P' ? 0 : +b),
+      a[1] +
+        b *
+          (c == 'A+'
+            ? 4.5
+            : c == 'A0'
+            ? 4.0
+            : c == 'B+'
+            ? 3.5
+            : c == 'B0'
+            ? 3.0
+            : c == 'C+'
+            ? 2.5
+            : c == 'C0'
+            ? 2.0
+            : c == 'D+'
+            ? 1.5
+            : c == 'D0'
+            ? 1.0
+            : 0.0),
+    ],
+    [0, 0],
+  ).reduce((a, b) => b / a),
 )
