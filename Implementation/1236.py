cd8b48384a71273c6f293de833aaa9c6e0a6cc0f
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2023-03-04 13:33:15 +0900

    solved : 1236

--- Implementation/1236.py
+++ Implementation/1236.py
@@ -11,3 +11,9 @@ for j in range(M) :
         if i == N - 1 :
             B.append(j)
 print(max(len(A), len(B)))
+# N,M=map(int, input().split())
+# I=[input() for _ in range(N)]
+# F=lambda x: not'X'in x
+# c=sum(map(F, I))
+# r=sum(map(F, zip(*I)))
+# print(max(r, c))
--- example.py
+++ example.py
@@ -1,13 +1,6 @@
-N,M = map(int,input().split())
-I = []
-for _ in range(N) : I.append(input())
-A,B = [],[]
-for i in range(N) :  
-    if all(x == '.' for x in I[i]) :
-        A.append(i)
-for j in range(M) :  
-    for i in range(N) :
-        if I[i][j] == 'X' : break
-        if i == N - 1 :
-            B.append(j)
-print(max(len(A), len(B)))
+N,M=map(int, input().split())
+I=[input() for _ in range(N)]
+F=lambda x: not'X'in x
+c=sum(map(F, I))
+r=sum(map(F, zip(*I)))
+print(max(r, c))
