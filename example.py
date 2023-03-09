ac2e9216fd20880aa7eb69da1dd95c0da942286a
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2023-03-09 11:10:29 +0900

    solved : 13241

--- example.py
+++ example.py
@@ -1,2 +1,15 @@
-a,b = map(int, input().split(), [2,2])
-print(f'{a + b:b}')
+I = []
+while (1) :
+    x = input()
+    if x == '0' : break
+    I.append(x)
+O = []
+for n in I:
+    l = len(n)
+    o = l + 1
+    for i in range(l):
+        if n[i] == '0' : o += 4
+        elif n[i] == '1' : o += 2
+        else : o += 3
+    O.append(o)
+for o in O: print(o)
