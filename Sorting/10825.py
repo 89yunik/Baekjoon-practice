0e136d00322ec9013dd80bc64c256529b7b3b5dc
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2023-03-11 22:22:22 +0900

    solved : 1371 - python 끝이 유동적인 data 입력 받기

--- Sorting/10825.py
+++ Sorting/10825.py
@@ -1,3 +1,2 @@
 N = int(input())
-I = sorted([input().split() for _ in range(N)], key=lambda x:(-int(x[1]), int(x[2]), -int(x[3]), x[0]))
-for i in I : print(i[0])
+print(*[i[0] for i in sorted([input().split() for _ in range(N)], key=lambda x:(-int(x[1]), int(x[2]), -int(x[3]), x[0]))])
