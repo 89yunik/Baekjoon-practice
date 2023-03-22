9ab460065043486e84f95f49bff60a515f3a9a66
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2023-03-22 16:36:58 +0900

    solved : 1598

--- example.js
+++ example.js
@@ -1,43 +1,8 @@
 let [, ...I] = `${require('fs').readFileSync('./dev/stdin')}`.trim()
-  .split`\n`.map(e => e.split` `.map(Number))
-let [i, O] = [0, []]
-while (i < I.length) {
-  const [[n], A, [m]] = [I[i], I[i + 1], I[i + 2]]
-  i += 3
-  const [x, adj, deg, Q] = [
-    i + m,
-    [...Array(n + 1)].map(_ => Array(n + 1).fill(0)),
-    Array(n + 1).fill(0),
-    [],
-  ]
-  for (let j = 0; j < n; j++)
-    for (let k = j + 1; k < n; k++) {
-      adj[A[j]][A[k]]++
-      deg[A[k]]++
-    }
-  let o = []
-  for (; i < x; i++) {
-    const [a, b] = I[i]
-    if (adj[a][b]) {
-      adj[a][b]--
-      adj[b][a]++
-      deg[a]++
-      deg[b]--
-    } else {
-      adj[a][b]++
-      adj[b][a]--
-      deg[a]--
-      deg[b]++
-    }
-  }
-  deg.forEach((e, i) => i && !e && Q.push(i))
-  if (!o.length) {
-    while (Q.length) {
-      const c = Q.shift()
-      o.push(c)
-      adj[c].forEach((e, n) => e > 0 && !--deg[n] && Q.push(n))
-    }
-  }
-  O.push(o.length == n ? o.join` ` : 'IMPOSSIBLE')
+  .split`\n`.map(Number)
+function F(A) {
+  let [max, cnt] = [A[0], 1]
+  A.forEach(e => e > max && (max = e) && ++cnt)
+  return cnt
 }
-console.log(O.join`\n`)
+console.log(F(I), F(I.reverse()))
