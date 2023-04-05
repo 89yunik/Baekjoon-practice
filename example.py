864c1c06f5723b499017fa36236fc3d916435d92
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2023-04-05 18:06:14 +0900

    updated : 1816

--- example.py
+++ example.py
@@ -1,9 +1,6 @@
 N=int(input())
-S,max=[int(input()) for _ in range(N)],10**6+1
-A,pNums=[1]*max,[]
-for i in range(2,max):
-    if A[i]==1: pNums.append(i)
-    for j in range(i*2,max,i):A[j]=0
-for x in S:
-    if all(x%y > 0 for y in pNums): print('YES')
-    else: print('NO')
+for _ in range(N):
+    x,o=int(input()),'YES'
+    for i in range(2,1000001):
+        if x%i==0: o='NO'; break
+    print(o)
