e64305e0f518836e040e1d24d456b5fc30c88a22
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2023-04-10 15:36:41 +0900

    solved : 10164

--- example.py
+++ example.py
@@ -1,15 +1,12 @@
 N,M,K=map(int, input().split())
 o=0
-def num(N,M):
-    dp=[[1]*(M+1) for _ in range(N)]
-    for i in range(1,N):
-        for j in range(1,M):
-            dp[i][j]=dp[i-1][j]+dp[i][j-1]
-    return dp[N-1][M-1]
-if K==0:o=num(N,M)
+dp=[[1]*M for _ in range(N)]
+for i in range(1,N):
+    for j in range(1,M):
+        dp[i][j]=dp[i-1][j]+dp[i][j-1]
+if K==0:o=dp[N-1][M-1]
 else : 
-    r,c=K//M, K%M
-    if c==0: c=K
-    r+=1
-    o=num(r,c)*num(N-r+1, M-c+1)
+    r,c=(K-1)//M, K%M-1
+    if c==-1:c+=M
+    o=dp[r][c]*dp[N-1-r][M-1-c]
 print(o)
