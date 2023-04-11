2aa385c91759aef498e6183e800a6d12c7118f08
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2023-04-11 20:36:50 +0900

    solved : 1325

--- example.js
+++ example.js
@@ -1,23 +1,27 @@
-let [[n, m], ...I] = `${require('fs').readFileSync('./dev/stdin')}`.trim()
+let [[N], ...I] = `${require('fs').readFileSync('./dev/stdin')}`.trim()
   .split`\n`.map(e => e.split` `.map(Number))
-const V = [...Array(n)].map(_ => Array(m).fill(0))
-let O = [0, 0]
-for (let i = 0; i < n; i++)
-  for (let j = 0; j < m; j++)
-    if (!V[i][j] && I[i][j] && ++O[0]) {
-      let S = 0
-      function dfs(x, y) {
-        V[x][y]++
-        S++
-        const N = [
-          [x - 1, y],
-          [x, y - 1],
-          [x, y + 1],
-          [x + 1, y],
-        ]
-        N.forEach(([nx, ny]) => I[nx] && I[nx][ny] && !V[nx][ny] && dfs(nx, ny))
-      }
-      dfs(i, j)
-      O[1] = Math.max(O[1], S)
-    }
-console.log(O.join`\n`)
+// let N = 10000
+// let I = [...Array(100000)].map((_, i) => [i % N, (i + 1) % N])
+const [G, O] = [[...Array(N + 1)].map(_ => []), Array(N + 1).fill(0)]
+I.forEach(([A, B]) => G[B].push(A))
+function dfs(s) {
+  let [S, V, o] = [[s], Array(N + 1).fill(0), 0]
+  while (S.length) {
+    const c = S.pop()
+    V[c]++
+    o++
+    G[c].forEach(n => !V[n] && S.push(n))
+  }
+  return o
+}
+let max = 0
+for (let i = 1; i <= N; i++) {
+  O[i] = dfs(i)
+  max = Math.max(O[i], max)
+}
+console.log(
+  O.reduce((a, b, i) => {
+    if (b == max) a.push(i)
+    return a
+  }, []).join` `,
+)
