b72d8e5ca6b3dd35fb9b9022bfd66fd7dd6a57c7
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2023-09-03 16:17:14 +0900

    solved : 2485

--- example.py
+++ example.py
@@ -1,6 +1,3 @@
-N=int(input())
-dp=[4,6]
-for i in range(2,N): 
-    l=len(dp)
-    dp.append(dp[l-2]+dp[l-1])
-print(dp[N-1])
+T,*I = map(lambda x: x.split(), open('./dev/stdin').read().split('\n'))
+A = {'kg': [2.2046, 'lb'], 'lb': [0.4536, 'kg'], 'l': [0.2642, 'g'], 'g': [3.7854, 'l']}
+for x,y in I[:T]: print(f'{float(x)*A[y][0]:.4f} {A[y][1]}')
