a83c358b0cc3db9ee5a910d2808583ef0f4401c0
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2024-01-07 21:51:56 +0900

    solved : 13565

--- example.js
+++ example.js
@@ -1,6 +1,4 @@
-let [N, ...I] = `${require('fs').readFileSync('./dev/stdin')}`.trim()
-  .split`\n`.map(Number)
-gcd = (a, b) => (b ? gcd(b, a % b) : a)
-let dist = 0
-for (let i in I) dist = gcd(I[+i + 1] - I[i], dist)
-console.log((I[N - 1] - I[0]) / dist + 1 - N)
+let [[a1, a0], [c], [n0]] = `${require('fs').readFileSync(
+  './dev/stdin',
+)}`.trim().split`\r\n`.map(e => e.split` `.map(Number))
+// a1*n+a0<=c*
