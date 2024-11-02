cbe2752f6b48e77893c7b98282287d4bff26c362
Nick Yoon <89yunik@gmail.com>
2024-11-02 16:21:28 +0900

    solved : 1283.kt

--- example.js
+++ example.js
@@ -1,4 +1,17 @@
-let [[a1, a0], [c], [n0]] = `${require('fs').readFileSync(
-  './dev/stdin',
-)}`.trim().split`\r\n`.map(e => e.split` `.map(Number))
+// let [[a1, a0], [c], [n0]] = `${require('fs').readFileSync(
+//   './dev/stdin',
+// )}`.trim().split`\r\n`.map(e => e.split` `.map(Number))
 // a1*n+a0<=c*
+async function f() {
+  gameId = '161936'
+  const response = await fetch(
+    // `https://boardgamegeek.com//xmlapi2/search?query=${gameId}&type=boardgame`,
+    `https://boardgamegeek.com/xmlapi2/thing?id=${gameId}&stats=1`,
+  )
+  const text = await response.text()
+  //   const parser = new DOMParser()
+  //   const xml = parser.parseFromString(text, 'application/xml')
+  //   const imageUrl = xml.querySelector('image')?.textContent
+  console.log(text)
+}
+f()
--- example.py
+++ example.py
@@ -1,10 +1,1 @@
-M,N=[*map(int,input().split())]
-convert=['zero','one','two','three','four','five','six','seven','eight','nine']
-O=[]
-for i in range(M, N+1):
-    num_str = str(i)
-    converted_num = ' '.join([convert[int(num)] for num in num_str])
-    O.append([i,converted_num])
-O=[*map(lambda x:x[0], sorted(O, key=lambda x:x[1]))]
-
-for i in range(0,len(O),10): print(*O[i:i+10])
+N = int(input())
